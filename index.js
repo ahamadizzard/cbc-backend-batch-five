6b3a76d2186c54ade98d5ae16238717c71272547
ahamadizzard <ahamadizzard@hotmail.com>
2025-07-24 21:23:21 +0530

    changed the localhost port number

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000; // set port to environment variable or 50
 // Allow requests from your Vercel frontend's domain
 const allowedOrigins = [
   "https://central-beauty-cosmetics.vercel.app", // Replace with your actual Vercel domain
-  "http://localhost:3000", // Keep this for local development if your frontend runs on 3000
+  "http://localhost:5173", // Keep this for local development if your frontend runs on 3000
 ];
 
 const corsOptions = {
